91d8c00b9722c954add9f2bf01a9d48652447325
vikram0888 <vikramjha753@gmail.com>
2024-02-03 21:17:21 +0530

    data ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,1 +1,19 @@
 #data ignestion
+import os
+import sys
+from src.logger import logging
+from src.exception import CustomException
+import pandas as pd 
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+#intialize the data ingestion configuration
+@dataclass
+class DataIngestionConfig:
+    train_data_path=os.path.join('artifacts','train.csv')
+    test_data_path=os.path.join('artifacts','test.csv')
+    raw_data_path=os.path.join('artifacts','raw.csv')
+
+#create a data ingestsion class
+class DataIngestion:
+            
